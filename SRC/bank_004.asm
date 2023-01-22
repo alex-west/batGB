674d59cb2516555ade49b022ad144fe4473367ab
alex-west <rt55j1@gmail.com>
2023-01-21 23:47:35 -0800

    Renamed graphics files
    Tilemap Studio prefers ".2bpp" as an extension over ".chr". Also, changed the naming scheme so BG and sprite tiles pertaining to a level are next to each other.

--- SRC/bank_004.asm
+++ SRC/bank_004.asm
@@ -18,16 +18,16 @@ SECTION "ROM Bank $004", ROMX[$4000], BANK[$4]
     dw $8A00, res_4_6120
 
 res_4_402C: incbin "../packed/blank.bin"
-res_4_406D: incbin "../packed/spr_joker.bin"
-res_4_437C: incbin "../packed/spr_batwing.bin"
-res_4_4705: incbin "../packed/spr_batwingBossA.bin"
-res_4_497B: incbin "../packed/spr_batwingEnemiesA.bin"
-res_4_4E8B: incbin "../packed/spr_batwingBossB.bin"
-res_4_5100: incbin "../packed/spr_batwingEnemiesB.bin"
-res_4_5561: incbin "../packed/spr_streetsEnemies.bin"
-res_4_58C4: incbin "../packed/spr_factoryEnemies.bin"
-res_4_5C7B: incbin "../packed/spr_museumEnemies.bin"
-res_4_6120: incbin "../packed/spr_cathedralEnemies.bin"
+res_4_406D: incbin "../packed/vsJoker_boss.bin"
+res_4_437C: incbin "../packed/player_batwing.bin"
+res_4_4705: incbin "../packed/batwing_boss1.bin"
+res_4_497B: incbin "../packed/batwing_enemies1.bin"
+res_4_4E8B: incbin "../packed/batwing_boss2.bin"
+res_4_5100: incbin "../packed/batwing_enemies2.bin"
+res_4_5561: incbin "../packed/streets_enemies.bin"
+res_4_58C4: incbin "../packed/factory_enemies.bin"
+res_4_5C7B: incbin "../packed/museum_enemies.bin"
+res_4_6120: incbin "../packed/cathedral_enemies.bin"
 
 bank4_freespace:
 ; EoF
